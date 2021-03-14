10d0de1e26477b91e77793a5f7f994c29eff9945
Keivan <keivan_mousavi@hotmail.com>
2021-03-14 22:39:21 +0330

    add page for show frame

--- BurglarAlarm.Web/Controllers/HomeController.cs
+++ BurglarAlarm.Web/Controllers/HomeController.cs
@@ -11,27 +11,23 @@ namespace BurglarAlarm.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<ActionResult> ShowFrameIndex()
         {
-            return View();
-        }
+            return await Task.Run(() =>
+            {
+                if (OnlineModel.Frame != null)
+                {
+                    ViewData.Model =  OnlineModel.Frame.ToString();
+                }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                return View();
+            });
         }
     }
 }
