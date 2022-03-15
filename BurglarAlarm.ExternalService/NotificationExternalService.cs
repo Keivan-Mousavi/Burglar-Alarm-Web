87579b9f32d569ba40f0974bf78222ed0092d6dc
keivan mousavi <keivan_mousavi@hotmail.com>
2022-03-15 21:43:16 +0330

    fix bug

--- BurglarAlarm.ExternalService/NotificationExternalService.cs
+++ BurglarAlarm.ExternalService/NotificationExternalService.cs
@@ -14,60 +14,49 @@ namespace BurglarAlarm.ExternalService
     {
         public async Task<bool> SendNotification(string serial, AppSetting appSetting)
         {
-            return await Task.Run(() =>
+            foreach (var deviceId in ListDeviceIdNotification.ListDeviceId)
             {
-                try
+                WebRequest tRequest = WebRequest.Create(requestUriString: appSetting.Notifications.URL);
+                tRequest.Method = "post";
+                tRequest.ContentType = "application/json";
+
+                var data = new
                 {
-                    foreach (var deviceId in ListDeviceIdNotification.ListDeviceId)
+                    to = deviceId,
+                    notification = new
                     {
-                        WebRequest tRequest = WebRequest.Create(requestUriString: appSetting.Notifications.URL);
-                        tRequest.Method = "post";
-                        tRequest.ContentType = "application/json";
+                        body = "Security systems activated. Your TV is on",
+                        title = "Warning",
+                        icon = "icon"
+                    }
+                };
 
-                        var data = new
+                var json = JsonSerializer.Serialize(data);
+
+                Byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+                tRequest.Headers.Add(string.Format("Authorization: key={0}", appSetting.Notifications.ApplicationID));
+                tRequest.Headers.Add(string.Format("Sender: id={0}", appSetting.Notifications.SenderId));
+                tRequest.ContentLength = byteArray.Length;
+
+                using (Stream dataStream = await tRequest.GetRequestStreamAsync())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+
+                    using (WebResponse tResponse = await tRequest.GetResponseAsync())
+                    {
+                        using (Stream dataStreamResponse = tResponse.GetResponseStream())
                         {
-                            to = deviceId,
-                            notification = new
+                            using (StreamReader tReader = new StreamReader(dataStreamResponse))
                             {
-                                body = "Security systems activated. Your TV is on",
-                                title = "Warning",
-                                icon = "icon"
-                            }
-                        };
-
-                        var json = JsonSerializer.Serialize(data);
-
-                        Byte[] byteArray = Encoding.UTF8.GetBytes(json);
-
-                        tRequest.Headers.Add(string.Format("Authorization: key={0}", appSetting.Notifications.ApplicationID));
-                        tRequest.Headers.Add(string.Format("Sender: id={0}", appSetting.Notifications.SenderId));
-                        tRequest.ContentLength = byteArray.Length;
-
-                        using (Stream dataStream = tRequest.GetRequestStream())
-                        {
-                            dataStream.Write(byteArray, 0, byteArray.Length);
-
-                            using (WebResponse tResponse = tRequest.GetResponse())
-                            {
-                                using (Stream dataStreamResponse = tResponse.GetResponseStream())
-                                {
-                                    using (StreamReader tReader = new StreamReader(dataStreamResponse))
-                                    {
-                                        string sResponseFromServer = tReader.ReadToEnd();
-                                    }
-                                }
+                                string sResponseFromServer = await tReader.ReadToEndAsync();
                             }
                         }
                     }
-
-                    return true;
                 }
-                catch
-                {
-                    return false;
-                }
-            });
+            }
 
+            return true;
         }
     }
 }
